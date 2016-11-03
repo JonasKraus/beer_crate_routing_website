9237912cd8f3b4e172ab9b1eb2b8eb8becbbb501
JonasKraus <jonas.kraus@uni-ulm.de>
2016-11-03 15:39:44 +0100

    chnaged log

--- scripts/getProgressUpdate.php
+++ scripts/getProgressUpdate.php
@@ -12,14 +12,41 @@ $method = null;
 
 
 if (isset($_POST['ps']) && isset($_POST['vr']) && isset($_SERVER ['HTTP_USER_AGENT']) && $_SERVER ['HTTP_USER_AGENT'] == 'UnityPlayer/5.3.4f1 (UnityWebRequest/1.0, libcurl/7.38.0-DEV)') {
-    $method = "POST";
+
     $versionFromRequest = strtolower($_POST['vr']) == databaseConstants::getVERSIONSIMNAME()
         ? databaseConstants::getVERSIONSIM()
         : databaseConstants::getVERSIONCOMIC();
+
     $pseudonym = $_POST['ps'];
 
+    writeLog("request: version->" . $versionFromRequest . " pseudonym->" . $pseudonym . " user-agent->" . $_SERVER ['HTTP_USER_AGENT']);
 
-    writeLog("request: version->" . $versionFromRequest . " pseudonym->" . $pseudonym . " user-agent->" . $_SERVER ['User-Agent-x']);
+    setcookie("beercrate_routing_pseudonym", $pseudonym, time() + (86400 * 7), ";path=/dijkstra-studie"); // Cookie for 7 days
+
+    $progress = null;
+
+    try {
+
+        $database = new databaseManager();
+
+        $user = $database->getUser($pseudonym);
+        $user = json_decode($user);
+
+        $success = $database->getProgressUpdate($pseudonym, $user->version, $versionFromRequest);
+
+        if (!$success) {
+            $responseStatus = '200';
+        }
+
+    } catch (PDOException $e) {
+        header($_SERVER['SERVER_PROTOCOL'].' '.$responseStatus);
+        header('Content-type: text/html; charset=utf-8');
+        exit();
+    }
+
+    header($_SERVER['SERVER_PROTOCOL'].' '.$responseStatus);
+    header('Content-type: text/html; charset=utf-8');
+
 } else {
 
     writeLog("request: version->" . $_POST['vr'] . " pseudonym->" . $_POST['ps'] . " user-agent->" . $_SERVER ['HTTP_USER_AGENT']);
@@ -29,31 +56,6 @@ if (isset($_POST['ps']) && isset($_POST['vr']) && isset($_SERVER ['HTTP_USER_AGE
     exit();
 }
 
-setcookie("beercrate_routing_pseudonym", $pseudonym, time() + (86400 * 7), ";path=/dijkstra-studie"); // Cookie for 7 days
-
-$progress = null;
-
-try {
-
-    $database = new databaseManager();
-
-    $user = $database->getUser($pseudonym);
-    $user = json_decode($user);
-
-    $success = $database->getProgressUpdate($pseudonym, $user->version, $versionFromRequest);
-
-    if (!$success) {
-        $responseStatus = '200';
-    }
-
-} catch (PDOException $e) {
-    header($_SERVER['SERVER_PROTOCOL'].' '.$responseStatus);
-    header('Content-type: text/html; charset=utf-8');
-    exit();
-}
-
-header($_SERVER['SERVER_PROTOCOL'].' '.$responseStatus);
-header('Content-type: text/html; charset=utf-8');
 
 function writeLog ($message) {
 
