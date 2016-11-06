65eeca8e825b6c1942fefb5f709e4092120141cd
JonasKraus <jonas.kraus@uni-ulm.de>
2016-11-06 15:06:42 +0100

    added check for ip

--- scripts/updateLinkFromLimesurvey.php
+++ scripts/updateLinkFromLimesurvey.php
@@ -1,3 +1,29 @@
 <?php
 
-echo $_SERVER['HTTP_REFERER'];
+var_dump($_SERVER['HTTP_REFERER']);
+
+if (!isset($_SERVER['HTTP_REFERER'])) {
+    echo "nicht gesetzt";
+}
+
+echo "---------------<br>";
+var_dump(get_client_ip());
+
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
