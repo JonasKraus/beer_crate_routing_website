48b2ef0e4dcf965d08fd0a5398ed6b574dadd6d9
JonasKraus <jonas.kraus@uni-ulm.de>
2016-11-13 13:55:29 +0100

    fixed date display

--- scripts/index.js
+++ scripts/index.js
@@ -126,7 +126,7 @@ function setBreadcrumps () {
                     if (Date.parse(pause_survey_till) > Date.parse(new Date())) {
                         var dateFormatted =
                             new Date(pause_survey_till).getDate() + "."
-                            + (new Date(pause_survey_till).getMonth() + 1) + "."
+                            + new Date(pause_survey_till).getMonth() + "."
                             + new Date(pause_survey_till).getYear();
 
                         showSnackbar("Du kannst erst ab dem " + dateFormatted + " weiter machen.");
