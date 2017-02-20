e64d00aea1fb40fefab4107c9a2038970d2ce68e
JonasKraus <jonas.kraus@uni-ulm.de>
2017-02-20 22:28:19 +0100

    added tracking download php file

---
+++ scripts/trackDownload.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonas-uni
+ * Date: 20.02.2017
+ * Time: 22:22
+ */
+
+var_dump($_POST);
+
+$fileName = "game.zip";
+$file = "../download/zuox02z0weq/game.zip";
+
+if(!file_exists($file)) die("I'm sorry, the file doesn't seem to exist.");
+
+$type = filetype($file);
+// Get a date and timestamp
+$today = date("F j, Y, g:i a");
+$time = time();
+// Send file headers
+header("Content-type: $type");
+header("Content-Disposition: attachment;filename=". $fileName);
+header("Content-Transfer-Encoding: binary");
+header('Pragma: no-cache');
+header('Expires: 0');
+// Send the file contents.
+set_time_limit(0);
+readfile($file);
